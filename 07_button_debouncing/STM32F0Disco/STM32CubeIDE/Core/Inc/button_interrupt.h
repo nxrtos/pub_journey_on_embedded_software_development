fe0221f97953b9be20cfe2d7e169becab03a66e9
Blue Now <bluenow1896@gmail.com>
2021-12-08 18:59:15 -0500

    create 07_button_debouncing/  to demo button deboucing ,input flow_control

---
+++ 07_button_debouncing/STM32F0Disco/STM32CubeIDE/Core/Inc/button_interrupt.h
@@ -0,0 +1,31 @@
+/**
+  ******************************************************************************
+  * @file    button_interrupt.h
+  * @brief   This file contains head of button_interrupt
+  ******************************************************************************
+  * @attention
+  *
+  *
+ ******************************************************************************
+  */
+
+#ifndef __BUTTON_INTERRUPT_H
+#define __BUTTON_INTERRUPT_H
+
+#include "stm32f051_discovery.h"
+#include  "systick_timer.h"
+
+extern
+volatile uint32_t user_button_state , user_button_changed;
+extern  SYSTick_Timer_t  button_debounce_timer;
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+extern void init_button_state(void);
+extern void button_debouce_followup_process(void);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __BUTTON_INTERRUPT_H */
---
+++ 07_button_debouncing/STM32F4Disco/STM32CubeIDE/Core/Src/button_interrupt.c
@@ -0,0 +1,76 @@
+/**
+  ******************************************************************************
+  * @file    button_interrupt.c
+  * @brief   This file contains source of button_interrupt
+  ******************************************************************************
+  * @attention
+  *
+  *
+ ******************************************************************************
+  */
+
+#include  "button_interrupt.h"
+
+#define  BUTTON_DEBOUNCE_TICKS		10
+
+volatile uint32_t user_button_state , user_button_changed;
+uint32_t prev_state ;
+SYSTick_Timer_t  button_debounce_timer;
+
+void init_button_state()
+{
+  user_button_state = BSP_PB_GetState(BUTTON_KEY);
+  prev_state = user_button_state;
+  user_button_changed = 0;
+  stop_systick_timer(&button_debounce_timer);
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  if(KEY_BUTTON_PIN == GPIO_Pin)
+  {
+	  user_button_state = BSP_PB_GetState(BUTTON_KEY);
+	  if(prev_state != user_button_state)
+	  {
+	    user_button_changed = 1;
+	    prev_state = user_button_state;
+	    // disable IRQ
+	    HAL_NVIC_DisableIRQ(EXTI0_IRQn);
+	    // start debounce_timer
+	    start_systick_timer(&button_debounce_timer, BUTTON_DEBOUNCE_TICKS);
+	  }
+  }
+}
+
+void button_debouce_followup_process(void)
+{
+  stop_systick_timer(&button_debounce_timer);
+  user_button_state = BSP_PB_GetState(BUTTON_KEY);
+  if(prev_state != user_button_state)
+  {
+    user_button_changed = 1;
+	prev_state = user_button_state;
+	// disable IRQ
+	HAL_NVIC_DisableIRQ(EXTI0_IRQn);
+	// start debounce_timer
+	start_systick_timer(&button_debounce_timer, BUTTON_DEBOUNCE_TICKS);
+  }
+  else
+  {
+    stop_systick_timer(&button_debounce_timer);
+    // re-enable IRQ
+    HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+    // it may double check button state after enable IRQ
+    // to ensure no miss button state change
+    user_button_state = BSP_PB_GetState(BUTTON_KEY);
+    if(prev_state != user_button_state)
+    {
+      user_button_changed = 1;
+      prev_state = user_button_state;
+      // disable IRQ
+      HAL_NVIC_DisableIRQ(EXTI0_IRQn);
+      // start debounce_timer
+      start_systick_timer(&button_debounce_timer, BUTTON_DEBOUNCE_TICKS);
+    }
+  }
+}
